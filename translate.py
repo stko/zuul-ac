010b898c3ca3287e12ed0863c8626bd103fb7bd5
Steffen Köhler <steffen@koehlers.de>
2020-04-17 15:49:48 +0200

    improved i18n support

--- translate.py
+++ translate.py
@@ -2,14 +2,17 @@
 # -*- coding: utf-8 -*-
 
 # https://inventwithpython.com/blog/2014/12/20/translate-your-python-3-program-with-the-gettext-module/
-'''
+
+import os
 import gettext
 localedir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'locale')
-translate = gettext.translation('guess', localedir, fallback=True)
-_ = translate.gettext
-'''
+# translate = gettext.translation('zuul-ac', localedir, languages=['de'], fallback=True)
+# _ = translate.gettext
+gettext.install('zuul-ac',localedir)
+
+print(_("Main Menu"),"verdammt...",localedir)
 
 def gettext(text):
-	#global translate
-	#return translate.gettext(text)
-	return text
+	global translate
+	return translate.gettext(text)
+	#return text
