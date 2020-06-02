5203c743106afd5c108bea276b943b00cf3bbb47
Steffen Köhler <steffen@koehlers.de>
2020-06-02 12:53:33 +0200

    change mqtt message format
    change mqtt message from simple plain QRcode to JSON to allow  e.g. an additional doorID in case that a building have several doors and so QR readers at different location to enable that the right doors is been opened

--- qr_to_mqtt.py
+++ qr_to_mqtt.py
@@ -9,6 +9,7 @@
 #	zbarcam | python3 qr_to_mqtt.py
 
 import sys
+import json
 
 import paho.mqtt.client as mqtt
 
@@ -18,13 +19,15 @@ def on_connect(client, userdata, flags, rc):
 client = mqtt.Client()
 client.on_connect = on_connect
 
-client.connect("localhost", 1883, 60)
+client.connect("10.48.164.82", 1883, 60)
 
 client.loop_start()
 
 for line in sys.stdin:
 	elements=line.strip().split(':')
 	if elements[0]=="QR-Code":
-		print(line)
-		client.publish("door/qrcode", ":".join(elements[1:]))
+		qrcode=":".join(elements[1:])
+		json_msg={'qrcode':qrcode,'doorid':'ATDT5912,#61'}
+		print(line, json.dumps(json_msg))
+		client.publish("door/qrcode", json.dumps(json_msg))
 
