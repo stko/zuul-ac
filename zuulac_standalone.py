fb085be7e6b42e9be017f93b6062190b4461d2f2
Steffen Köhler <steffen@koehlers.de>
2025-12-01 18:21:44 +0100

    latest tweaks; schematics now with QRCode part number & correct connector; QRCode Reading is working

--- zuulac_standalone.py
+++ zuulac_standalone.py
@@ -1,4 +1,10 @@
+"""Standalone ZUULAC worker for Raspberry Pi with serial connected QR code reader on /dev/serial0
+and GPIO controlled door relay on GPIO2."""
+
 import time
+import serial
+import RPi.GPIO as GPIO
+
 from zuulworker import ZuulWorker
 
 def otp_request(requestdata):
@@ -11,27 +17,45 @@ def otp_request(requestdata):
         "config": {
             "result": True,
             "msg": "This pin {1} is valid for {0} seconds",
-            "type": "text",
+            "type": "qrcode",
             "keypadchars": "1234567890",
-            "length": 6,
+            "length": 10,
             "valid_time": 30
         }
     }
     return response
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(2,GPIO.OUT) #led
+
+# Initialize serial connection to QR code reader
+# maybe there's some trouble with permissions, run 'sudo usermod -a -G dialout $USER' and reboot if you have issues
+# also see in case of trouble: https://forums.raspberrypi.com/viewtopic.php?t=299548
+ser = serial.Serial('/dev/serial0', 9600, timeout=1)
+ser.flush()
+
 def get_input()->str:
     """In which ever way you want to get the user input, e.g. from a keypad, QRCode-Reader etc."""
-    user_input = input("Enter the Pin: ")
-    print()
-    return user_input.strip()
+    # Example for reading from a serial connected QR-Code reader
+
+    while True:
+        qr_code_value = ser.readline().decode("utf-8", errors="ignore").strip()
+        if qr_code_value:
+           print("QR Code detected:", qr_code_value)
+           return qr_code_value
+        time.sleep(0.1)
+
 
 def open_door(allow_open:bool):
     """ this function is called when zuul detects a valid user attempt """
     if allow_open:
         print ("Open the door")
         # door opening mechanism here
-        time.sleep(2)  # simulate door open time
+        GPIO.output(2, GPIO.HIGH)  # activate relay to open door
+        time.sleep(3)  # simulate door open time
         print ("Close the door")
+        GPIO.output(2, GPIO.LOW)  # de activate relay to open door
+
     else:
         print ("Do not open the door")
 
